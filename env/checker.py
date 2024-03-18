1f3d41c6e4ba85966c73725c2364d307eec8e316
ZhiqiangTu <2623036785@qq.com>
2024-03-18 17:14:13 +0800

    [feature] 完成check模块设计

--- env/checker.py
+++ env/checker.py
@@ -7,4 +7,24 @@ def check():
         print("fuzzywuzzy has not been installed..")
         quit()
 
+    # check selenium
+    try:
+        import selenium
+    except ImportError:
+        print("selenium has not been installed..")
+        quit()
+
+    # request
+    try:
+        import requests
+    except ImportError:
+        print("requests has not been installed..")
+        quit()
+
+    # beautifulsoup4
+    try:
+        import bs4
+    except ImportError:
+        print("beautifulsoup4 has not been installed..")
+        quit()
 
